0247876271ea2db6dfe8f3d678a9e667a7796510
ToRxmrig <79212597+ToRxmrig@users.noreply.github.com>
2024-09-14 16:35:17 -0500

    Update Deploy.s.sol

--- contract/script/Deploy.s.sol
+++ contract/script/Deploy.s.sol
@@ -15,8 +15,8 @@ contract Deploy is Script {
 
     function run() public{
         address sandwich = HuffDeployer.broadcast("sandwich");
-        bytes32 salt = bytes32(0x91bf9f374fd56cc3c421f9c988af6e5c4a61df8262bc4795a02024e288e38303);
-        vm.broadcast(0x91BF9F374fD56CC3C421f9C988af6E5C4A61DF82);
+        bytes32 salt = bytes32(0xE9D7B0C91a318ED1D016C51b7CC55D9a7A2ADaB562bc4795a02024e288e38303);
+        vm.broadcast(0xE9D7B0C91a318ED1D016C51b7CC55D9a7A2ADaB5);
         address metamorphicContract = factory.deployMetamorphicContractFromExistingImplementation(salt, sandwich, "");
         console.log(metamorphicContract);
     }
